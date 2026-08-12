a9a1dbfd1bf5880a5b27dce64b3b1d8e83721543
Karan Yadure <karanyadure@gmail.com>
2026-08-12 14:20:50 +0530

    Day 1: scaffold Flask app, render basic form template

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+
+app= Flask(__name__)
+
+def build_playload(form):
+
+    return ""
+
+def make_qr_image(playload, fg="#16171A", bg="#FFFFFF", ecc="M"):
+
+    raise NotImplementedError
+
+def image_to_data_url(img):
+
+    raise NotImplementedError
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    context={
+    "qr_type": "link",
+    "form_values": {},
+    "image_data_url": None,
+    "error": None,
+    }
+
+    if request.method=="POST":
+        pass
+
+    return render_template("index.html",**context)
+
+if __name__ == "__main__":
+    app.run(debug=True)
